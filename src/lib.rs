521c17bbf10f141f6a3eb999e0e4139933ff0ad8
John <johnteee@gmail.com>
2021-08-03 22:51:23 +0800

    Clean up codes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,32 @@
+// Crates
+
 extern crate bytes;
-#[cfg(feature = "multipart")]
-extern crate formdata;
 extern crate futures;
 extern crate http;
+extern crate url;
+
 #[cfg(feature = "for_hyper")]
 extern crate hyper;
+#[cfg(feature = "for_hyper")]
+extern crate tokio;
+
+#[cfg(feature = "multipart")]
+extern crate formdata;
 #[cfg(feature = "multipart")]
 extern crate mime;
 #[cfg(feature = "multipart")]
 extern crate multer;
+
 #[cfg(feature = "for_serde")]
 extern crate serde;
 #[cfg(feature = "for_serde")]
 extern crate serde_json;
-#[cfg(feature = "for_hyper")]
-extern crate tokio;
-extern crate url;
 
-#[cfg(feature = "for_hyper")]
-pub mod bind_hyper;
+// MODs
 
 pub mod common;
 pub mod simple_api;
 pub mod simple_http;
+
+#[cfg(feature = "for_hyper")]
+pub mod bind_hyper;
